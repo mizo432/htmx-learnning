34f119159b6b2ed51bdd22ff465b7d545ef904e6
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2024-09-26 11:35:52 +0900

    Remove unused compileOnly configuration
    The compileOnly configuration which extends from annotationProcessor was not being used and has been removed. The necessary annotationProcessor dependency for Lombok has been explicitly added to ensure the build process remains intact. This cleanup simplifies the build script and avoids unnecessary configuration.

--- htmx-application/build.gradle.kts
+++ htmx-application/build.gradle.kts
@@ -13,12 +13,6 @@ java {
 	}
 }
 
-configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
-}
-
 repositories {
 	mavenCentral()
 	maven { url = uri("https://repo.spring.io/milestone") }
@@ -33,13 +27,13 @@ dependencies {
 	implementation("org.flywaydb:flyway-database-postgresql")
 	implementation("org.springframework:spring-jdbc")
 	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
 	runtimeOnly("org.postgresql:r2dbc-postgresql")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
-	// testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	testImplementation("org.assertj:assertj-core:3.26.3")
 }
 
