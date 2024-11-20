fd03bbc6b19549a4b8512ee58c5df2b14dbad53c
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2024-11-21 04:28:25 +0900

    Add new endpoint to DummyResource
    Introduced a new GET endpoint at /02 in DummyResource to handle requests using Dummy02Query. This addition complements the existing endpoint, providing more functionality to the API.

--- htmx-application/src/main/java/org/venuspj/htmx/admin/infra/api/dummy/DummyResource.java
+++ htmx-application/src/main/java/org/venuspj/htmx/admin/infra/api/dummy/DummyResource.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.venuspj.htmx.admin.appl.query.dummy.Dummy02Query;
 import org.venuspj.htmx.admin.appl.query.dummy.DummyQuery;
 
 @RestController
@@ -13,10 +14,17 @@ public class DummyResource {
 
   private final DummyQuery dummyQuery;
 
+  private final Dummy02Query dummy02Query;
+
   @GetMapping
   public void execute() throws InterruptedException {
     dummyQuery.execute();
   }
 
+  @GetMapping(path = "/02")
+  public void execute2() throws InterruptedException {
+    dummy02Query.execute();
+
+  }
 
 }
